a8f2924aea6d38f8877f115c12c4d76b00544e76
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-02-25 12:17:27 +0900

    Mutation

--- client/src/Hello.tsx
+++ client/src/Hello.tsx
@@ -3,7 +3,38 @@ import { RouteComponentProps } from "react-router";
 import axios from "axios";
 import { GRAPH_QL_ENDPOINT, APPSYNC_API_KEY } from "./Constants";
 
+interface User {
+  id: string;
+  name: string;
+  createdAt: string;
+}
+
+const createUser = async (name: string) => {
+  await axios.post(
+    GRAPH_QL_ENDPOINT,
+    {
+      query: `
+            mutation {
+              createUser(input: {name: "${name}"}) {
+                id,
+                name,
+                createdAt
+              }
+            }
+            `
+    },
+    {
+      headers: {
+        "Content-Type": "application/graphql",
+        "x-api-key": APPSYNC_API_KEY
+      }
+    }
+  );
+};
+
 export default function Hello(_: RouteComponentProps) {
+  const [name, setName] = React.useState("");
+  const [listUser, setListUser] = React.useState([] as User[]);
   React.useEffect(() => {
     const init = async () => {
       const res = await axios.post(
@@ -27,8 +58,22 @@ export default function Hello(_: RouteComponentProps) {
         }
       );
       console.info("res", res.data.data.listUser);
+      setListUser(res.data.data.listUser);
     };
     init();
   }, []);
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h2>Hello</h2>
+      {listUser.map(user => {
+        return (
+          <p>
+            {user.id}: {user.name}
+          </p>
+        );
+      })}
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={() => createUser(name)}>CREATE USER</button>
+    </div>
+  );
 }
