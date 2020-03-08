5241313acecdbc8185623d76a2085f7804a56dad
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-03-09 07:56:50 +0900

    Introduce appsync client

--- client/src/Constants.ts
+++ client/src/Constants.ts
@@ -1,5 +1,6 @@
 declare const GRAPH_QL_ENDPOINT: string;
 
 const _GRAPH_QL_ENDPOINT = GRAPH_QL_ENDPOINT;
+const AWS_REGION = "ap-northeast-1";
 
-export { _GRAPH_QL_ENDPOINT as GRAPH_QL_ENDPOINT };
+export { _GRAPH_QL_ENDPOINT as GRAPH_QL_ENDPOINT, AWS_REGION };
---
+++ client/src/lib/AppSync.ts
@@ -0,0 +1,15 @@
+import AWSAppSyncClient, { AUTH_TYPE } from "aws-appsync";
+import { GRAPH_QL_ENDPOINT, AWS_REGION } from "../Constants";
+import { getTokenSilentry } from "./Auth0";
+
+export const client = async () => {
+  const token = await getTokenSilentry();
+  new AWSAppSyncClient({
+    url: GRAPH_QL_ENDPOINT,
+    region: AWS_REGION,
+    auth: {
+      type: AUTH_TYPE.OPENID_CONNECT,
+      jwtToken: token
+    }
+  });
+};
