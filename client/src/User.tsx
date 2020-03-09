202b106232509468d226e83238e32577fe7b99ed
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-03-09 13:16:15 +0900

    Use appsync client

--- client/src/User.tsx
+++ client/src/User.tsx
@@ -3,6 +3,10 @@ import { RouteComponentProps } from "react-router";
 import axios from "axios";
 import { GRAPH_QL_ENDPOINT } from "./Constants";
 import { getTokenSilentry } from "./lib/Auth0";
+import { client as AppSyncClient } from "./lib/AppSync";
+import { ApolloProvider } from "react-apollo";
+import { Rehydrated } from "aws-appsync-react";
+import AWSAppSyncClient from "aws-appsync";
 
 interface User {
   id: string;
@@ -39,55 +43,41 @@ const createUser = async (email: string, password: string) => {
 export default function User(_: RouteComponentProps) {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const [listUser, setListUser] = React.useState([] as User[]);
+  const [client, setClient] = React.useState(null as unknown);
   React.useEffect(() => {
     const init = async () => {
-      const token = await getTokenSilentry();
-      const res = await axios.post(
-        GRAPH_QL_ENDPOINT,
-        {
-          query: `
-            query {
-              listUser {
-                id,
-                providerId,
-                providerName,
-                createdAt
-              }
-            }
-            `
-        },
-        {
-          headers: {
-            "Content-Type": "application/graphql",
-            Authorization: token
-          }
-        }
-      );
-      console.info("res", res.data.data.listUser);
-      setListUser(res.data.data.listUser);
+      const appsyncClient = await AppSyncClient();
+      setClient(appsyncClient);
     };
     init();
   }, []);
+  if (!client) {
+    console.info("return null");
+    return null;
+  }
+  console.info("client", client);
+  const appClient = client as AWSAppSyncClient<any>;
   return (
-    <div>
-      <h2>User</h2>
-      {listUser.map(user => {
-        return (
-          <p>
-            {user.id} | {user.providerId} | {user.providerName} |{" "}
-            {user.createdAt}
-          </p>
-        );
-      })}
-      <div>
-        email: <input value={email} onChange={e => setEmail(e.target.value)} />
-      </div>
-      <div>
-        password:{" "}
-        <input value={password} onChange={e => setPassword(e.target.value)} />
-      </div>
-      <button onClick={() => createUser(email, password)}>CREATE USER</button>
-    </div>
+    <ApolloProvider client={appClient}>
+      <Rehydrated>
+        <div>
+          <h2>User</h2>
+          <div>
+            email:{" "}
+            <input value={email} onChange={e => setEmail(e.target.value)} />
+          </div>
+          <div>
+            password:{" "}
+            <input
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <button onClick={() => createUser(email, password)}>
+            CREATE USER
+          </button>
+        </div>
+      </Rehydrated>
+    </ApolloProvider>
   );
 }
