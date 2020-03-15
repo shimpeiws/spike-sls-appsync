9bca7da391937cf98d65c1df80fc84c2221990a3
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-03-16 07:20:55 +0900

    Use appsync client

--- client/src/Company.tsx
+++ client/src/Company.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import { RouteComponentProps } from "react-router";
-import axios from "axios";
-import { GRAPH_QL_ENDPOINT } from "./Constants";
-import { getTokenSilentry } from "./lib/Auth0";
+import gql from "graphql-tag";
+import { graphql, compose } from "react-apollo";
+import { graphqlMutation } from "aws-appsync-react";
 
 interface Company {
   id: string;
@@ -10,65 +9,38 @@ interface Company {
   createdAt: string;
 }
 
-const createCompany = async (name: string) => {
-  const token = await getTokenSilentry();
-  await axios.post(
-    GRAPH_QL_ENDPOINT,
-    {
-      query: `
-            mutation {
-              createCompany(input: {name: "${name}"}) {
-                id,
-                name,
-                createdAt
-              }
-            }
-            `
-    },
-    {
-      headers: {
-        "Content-Type": "application/graphql",
-        Authorization: token
-      }
+const listCompany = gql`
+  query {
+    listCompany {
+      id
+      name
+      createdAt
     }
-  );
-};
+  }
+`;
 
-export default function User(_: RouteComponentProps) {
+const createCompany = gql`
+  mutation createCompany($name: String!) {
+    createCompany(input: { name: $name }) {
+      id
+      name
+      createdAt
+    }
+  }
+`;
+
+interface Props {
+  companies: Company[];
+  createCompany: any;
+  data: any;
+}
+
+const Company: React.FC<Props> = props => {
   const [name, setName] = React.useState("");
-  const [listCompany, setListCompany] = React.useState([] as Company[]);
-  React.useEffect(() => {
-    const init = async () => {
-      const token = await getTokenSilentry();
-      const res = await axios.post(
-        GRAPH_QL_ENDPOINT,
-        {
-          query: `
-            query {
-              listCompany {
-                id,
-                name,
-                createdAt
-              }
-            }
-            `
-        },
-        {
-          headers: {
-            "Content-Type": "application/graphql",
-            Authorization: token
-          }
-        }
-      );
-      console.info("res", res.data.data.listCompany);
-      setListCompany(res.data.data.listCompany);
-    };
-    init();
-  }, []);
   return (
     <div>
       <h2>Company</h2>
-      {listCompany.map(company => {
+      {props.companies.map(company => {
         return (
           <p>
             {company.id}: {company.name}
@@ -76,7 +48,23 @@ export default function User(_: RouteComponentProps) {
         );
       })}
       <input value={name} onChange={e => setName(e.target.value)} />
-      <button onClick={() => createCompany(name)}>CREATE COMPANY</button>
+      <button onClick={() => props.createCompany({ name })}>
+        CREATE COMPANY
+      </button>
     </div>
   );
-}
+};
+
+export default compose(
+  graphql(listCompany, {
+    options: {
+      fetchPolicy: "cache-and-network"
+    },
+    props: (props: any) => ({
+      companies:
+        props.data && props.data.listCompany ? props.data.listCompany : [],
+      data: props.data
+    })
+  }),
+  graphqlMutation(createCompany, listCompany, "Company")
+)(Company);
